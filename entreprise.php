043b59e39b953beae9b638115a7723f81a9f7f67
YohannOUEDRAOGO <Yohannodg@gmail.com>
2025-03-30 22:53:34 +0100

    Changement nom de la base de donnée

--- entreprise.php
+++ entreprise.php
@@ -9,8 +9,8 @@ if (!isset($_SESSION['user'])) {
 
 $servername = "localhost";
 $username = "root";
-$password = ""; 
-$dbname = "projet-web";
+$password = "";
+$dbname = "gestion";
 
 try {
     $pdo = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
